473cb171f4e41f47261c1267fa2f090b02a493cf
booyaa <booyaabooyaabooyaa@gmail.com>
2015-09-19 19:15:52 +0100

    fix path

--- scripts/images2video.sh
+++ scripts/images2video.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 SCREENCAPS_DIR=~/Desktop/dockerhackday
-DATE_STAMP_MASK=$(date +"%Y%m%d")
+# DATE_STAMP_MASK=$(date +"%Y%m%d")
+DATE_STAMP_MASK=$1
 IMAGE_COUNT=$(ls $SCREENCAPS_DIR/$DATE_STAMP_MASK-*.png | wc -l)
 DATE_STAMP_DIR=$SCREENCAPS_DIR/$DATE_STAMP_MASK
 mkdir -p $DATE_STAMP_DIR/temp
 mv $SCREENCAPS_DIR/$DATE_STAMP_MASK*.png $DATE_STAMP_DIR
-convert -delay 4 $DATE_STAMP_DIR-*.png $DATE_STAMP_DIR/temp/%05d.jpg
+convert -delay 4 $DATE_STAMP_DIR/$DATE_STAMP_MASK-*.png $DATE_STAMP_DIR/temp/%05d.jpg
 ffmpeg -i $DATE_STAMP_DIR/temp/%05d.jpg -c:v libx264 -vf fps=25 -pix_fmt yuv420p $SCREENCAPS_DIR/dockerhackday-$DATE_STAMP_MASK.mp4
 
 # reference

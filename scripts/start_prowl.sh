442c56f50c287850e2ac50f4ff8d3d2353198bdd
booyaa <booyaabooyaabooyaa@gmail.com>
2015-09-23 17:18:07 +0100

    testing nohup

--- scripts/start_prowl.sh
+++ scripts/start_prowl.sh
@@ -10,11 +10,19 @@ echo start gotty
 # 2015/09/23 08:16:34 Connection closed: 127.0.0.1:57682
 # NOTE : I exit on disconnect is a bad UX, what if you're travelling and the
 # connection drops? Or your broadband isn't very reliable.
+# TODO : background and dump log file
 TMUX=tmux
 TMUX_COMMAND=attach
 # TMUX_OPTS="-r -t" # attach client as readonly to a named session
 TMUX_OPTS="-t" # attach client to a named session
-gotty --permit-write --once --index prowl.html $TMUX $TMUX_COMMAND $TMUX_OPTS $PROWL_SESSION
+nohup gotty --permit-write --once --index prowl.html $TMUX $TMUX_COMMAND $TMUX_OPTS $PROWL_SESSION
+
+# main loop
+while true
+do
+  echo hello
+  sleep 5
+done
 
 echo prowl has stopped
 

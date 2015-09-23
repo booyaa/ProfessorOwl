232068981eb475b6c7902896d4a6ec500e31bb00
booyaa <booyaabooyaabooyaa@gmail.com>
2015-09-23 08:24:50 +0100

    new startup script for prowl

--- scripts/start_prowl.sh
+++ scripts/start_prowl.sh
@@ -1,49 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash +x
+PROWL_SESSION=prowl
+echo prowl has started
+echo create prowl session
+tmux has $PROWL || tmux new -d -s prowl
 
-if [[ -z "$DO_PAT" ]]; then
-  echo 'DO_PAT environment var not found!'
-  echo 'You must provide Digital Ocean Personal Access Token!'
-  exit 1
-fi
+echo start gotty
+# TODO : client exit on client disconnect doesn't always work, trap for 
+# 2015/09/23 08:16:34 Command exited for: 127.0.0.1:57682
+# 2015/09/23 08:16:34 Connection closed: 127.0.0.1:57682
+# NOTE : I exit on disconnect is a bad UX, what if you're travelling and the
+# connection drops? Or your broadband isn't very reliable.
+TMUX=tmux
+TMUX_COMMAND=attach
+# TMUX_OPTS="-r -t" # attach client as readonly to a named session
+TMUX_OPTS="-t" # attach client to a named session
+gotty --permit-write --once --index prowl.html $TMUX $TMUX_COMMAND $TMUX_OPTS $PROWL_SESSION
 
-DOCKER_MACHINE=$(which docker-machine)
+echo prowl has stopped
 
-if [[ -z "$DOCKER_MACHINE" ]]; then
-  echo 'docker-machine not found!'
-  echo 'Download it from https://github.com/docker/machine/releases'
-  exit 1
-fi
-
-$DOCKER_MACHINE  create prowl \
-        --driver digitalocean \
-        --digitalocean-access-token $DO_PAT \
-        --digitalocean-region "sfo1" \
-        --digitalocean-size "1gb"
-
-PROWL_MACHINE_RESULT=$?
-
-if [[ ! $PROWL_MACHINE_RESULT -eq 0 ]]; then
-  echo 'Failed to create Professor Owl droplet, check Digital Ocean for system outages!'
-  exit 1
-fi
-
-cat <<SETUPPROWLUSER
-Congratulation you have created the Professor Owl machine! You now need to create
-your dedicated user account. Copy and paste the following instructions:
-
-$DOCKER_MACHINE ssh prowl
-useradd prowl -m -s /bin/bash
-adduser prowl docker
-passwd prowl
-apt-get update && apt-get install tmux curl
-mkdir /root/stage
-cd /root/stage
-curl -LO https://github.com/yudai/gotty/releases/download/pre-release/linux_amd64.tar.gz
-mv gotty /usr/local/bin
-chown root:root /usr/local/bin/gotty
-chmod a+x /usr/local/bin/gotty
-su -l prowl
-git clone https://github.com/booyaa/ProfessorOwl.git
-cd ProfessorOwl
-screen -c professorowl-screenrc
-SETUPPROWLUSER

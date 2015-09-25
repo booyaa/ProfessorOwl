b805c157260dadac68e5a728ffac63bc06b0b17c
booyaa <booyaabooyaabooyaa@gmail.com>
2015-09-25 19:14:03 +0100

    document tianon's handy docker run magick

--- scripts/start_prowl.sh
+++ scripts/start_prowl.sh
@@ -9,6 +9,20 @@ do
   echo create tmux session if it doesn\'t exist
   tmux has-session -t $PROWL_SESSION > /dev/null 2>&1 || tmux new-session -d -s $PROWL_SESSION docker-machine ssh dev
 
+  # this won't work for b2d setups, thanks tianon for docker run incantation
+  # tmux has-session -t $PROWL_SESSION > /dev/null 2>&1 || tmux new-session -d -s $PROWL_SESSION docker-machine ssh dev -- docker run -it --rm --privileged --net=host --pid=host -v /:/host debian:jessie chroot /host /bin/sh
+  #
+  # docker run \
+  # -it           \ # interactive and pseudo terminal
+  # --rm          \ # delete container on exit
+  # --privileged  \ # see all devices, more info http://blog.docker.com/2013/09/docker-can-now-run-within-docker/
+  # --net=host    \ # use host's network
+  # --pid=host    \ # use hosts's PID name space
+  # -v /:/host    \ # maps host's root to volume /host
+  # debian:jessie chroot /host /bin/sh # chroot the user into /host jail
+  # WARNING: this will allow student to edit your vps's root fs. need to make a custom chroot
+
+
   echo start gotty, logging to $GOTTY_LOG
   # TODO : client exit on client disconnect doesn't always work, trap for 
   # 2015/09/23 08:16:34 Command exited for: 127.0.0.1:57682

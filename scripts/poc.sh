b50d4a019840f532315ddcdd84e54ee48512e3d9
booyaa <booyaabooyaabooyaa@gmail.com>
2015-10-25 14:01:22 +0000

    switch to ajax for notifications

--- scripts/poc.sh
+++ scripts/poc.sh
@@ -1,83 +1,62 @@
 #!/usr/bin/env bash
-# further reading: http://www.tldp.org/LDP/abs/html/exit-status.html
-# http://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
+# further reading: 
+# - http://www.tldp.org/LDP/abs/html/exit-status.html
+# - http://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
 
+function say() {
+        echo "$@"
+	#FIXME hard coded hostname
+        curl --silent --request POST --data "message=$@" http://172.17.42.1:9999/say
+}
 
 while true
 do
-  CLIENTS=$(tmux lsc | grep -E 'prowl.*(ro)')
-  STUDENT_ID=${CLIENTS%:*}
+        while true
+        do
+          CLIENTS=$(tmux lsc | grep -E 'prowl')
+          STUDENT_ID=${CLIENTS%:*}
 
-  echo clients: $CLIENTS
-  echo student tty: $STUDENT_ID
+          # echo clients: $CLIENTS student tty: $STUDENT_ID
 
-  if [[ ! -z $STUDENT_ID ]]; then
-      break;
-  fi
+          if [[ ! -z $STUDENT_ID ]]; then
+              break;
+          fi
 
-  echo sleeping for 5 seconds...
-  sleep 5 
+          echo No connections found, sleeping for 5 seconds...
+          sleep 5
+        done
+
+        date
+        tmux send-keys -t prowl C-u
+        tmux send-keys -t prowl 'reset' C-m
+        tmux send-keys -t prowl 'clear' C-m
+        say 'Hi my name is Professor Owl, I am going to teach you how to Docker!'
+        sleep 10
+
+        say "Let's run the hello-world image"
+        sleep 10
+
+        echo run hello-world image
+        tmux send-keys -t prowl "docker run -it --rm hello-world" C-m
+        sleep 10
+
+        say "That was easy, wasn't it?"
+        sleep 10
+
+        say 'Be sure to come back and check on the progress of Professor Owl!'
+        sleep 30
+
+        say 'Ah so you are one of those people who stays to watch all the movie credits?'
+        sleep 5
+        say 'Enjoy :)'
+        sleep 5
+
+        tmux send-keys -t prowl "docker rm -f nc" C-m
+        tmux send-keys -t prowl "docker run -it --name nc supertest2014/nyan" C-m
+        sleep 30
+        tmux send-keys -t prowl C-c # OOB FTW!
+
+        echo about to sleep for 15 seconds, ctrl c would be a good idea now
+        sleep 15
+        echo restarting
 done
-
-# for testing
-# echo fall through
-# tmux kill-server
-# exit
-
-# clear
-tmux send-keys C-u
-tmux send-keys 'clear' C-m
-echo say hi
-tmux send-keys 'Hi my name is Professor Owl, I am going to teach you how to Docker!'
-sleep 5
-tmux send-keys C-u
-
-tmux send-keys "Let's run the hello-world image"
-sleep 5
-tmux send-keys C-u
-
-echo run hello-world image
-tmux send-keys "docker run -it --rm hello-world" C-m
-sleep 5
-
-echo say that was easy, wasnt it?
-tmux send-keys "That was easy, wasn't it?"
-tmux send-keys C-u
-
-echo now let the student have a go, removing lock
-tmux send-keys "Now your turn. DEBUG: lock off"
-sleep 5
-tmux send-keys C-u
-
-#TODO snap shot docker ps -a
-tmux switch-client -c $STUDENT_ID -r
-
-tmux display-message -c $STUDENT_ID "30 seconds left"
-echo 30 seconds
-sleep 10
-
-tmux display-message -c $STUDENT_ID "20 seconds left"
-echo 20 seconds
-sleep 10
-
-tmux display-message -c $STUDENT_ID "10 seconds left"
-echo 10 seconds
-sleep 5
-
-tmux display-message -c $STUDENT_ID "5 seconds left"
-echo 5 seconds
-
-# FIXME: still returns 0, despite local test after docker run returning 1
-echo User exit code: $?
-tmux switch-client -c $STUDENT_ID -r
-tmux send-keys C-u
-
-#TODO diff snap shot of docker ps -a, is there a new container is it based on hello-world?
-#TODO capture and display buffer. was the student successful?
-#TODO capture exit code of (vital you get this before you lock the screen)
-tmux send-keys "Well done, this concludes our lesson. DEBUG: lock on"
-sleep 15
-tmux send-keys C-u
-
-# this will kill gotty off
-tmux kill-server
